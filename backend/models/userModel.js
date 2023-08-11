c450c7a5c664f9b1f6ca911b47fa94d53a1f8735
Akash <akashkulkarni4444@gmail.com>
2023-08-11 22:03:52 +0530

    Backend Login and Signup

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     pic: {
       type: String,
@@ -14,6 +15,20 @@ const userSchema = mongoose.Schema(
   },
   { timestaps: true }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 
 const User = mongoose.model("User", userSchema);
 
